25e2ae3c2afc47225e6dcb59d159553ab15f8a85
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-09 10:04:51 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import './App.css'
+import './App.css';
 
 const company = {
-	name: "Google",
-	"numberOfEmployees": 6734,
-	"headquarters": "San Franscisco",
-	"logo": "googleLogo.png",
-	"homepage": "http://www.google.com"
+    name: 'Google',
+    numberOfEmployees: 6734,
+    headquarters: 'San Franscisco',
+    logo: 'googleLogo.png',
+    homepage: 'http://www.google.com'
 };
 
 function App() {
-
-	return (
-		<div className="App">
-			<div className="company">
-				<div className="name">Name: {company.name}</div>
-				<div className="numberOfEmployees">Employees: {company.numberOfEmployees}</div>
-				<div className="headquarters">Headquarters: {company.headquarters}</div>
-				<a href={company.homepage}>
-				<img src="/images/googleLogo.png" alt="the logo of Google"/>
-				</a>
-
-			</div>
-		</div>
-	)
+    return (
+        <div className="App">
+            <div className="company">
+                <div className="logo">
+                    <a href={company.homepage}>
+                        <img
+                            src="/images/googleLogo.png"
+                            alt="the logo of Google"
+                        />
+                    </a>
+                </div>
+                <div className="info">
+                    <div className="name">Name: {company.name}</div>
+                    <div className="numberOfEmployees">
+                        Employees: {company.numberOfEmployees}
+                    </div>
+                    <div className="headquarters">
+                        Headquarters: {company.headquarters}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
