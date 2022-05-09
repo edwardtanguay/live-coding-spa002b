85765a7dab50d451d549183a1663ffe742063ef7
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-09 09:53:42 +0200

    google logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
 
+const company = {
+	name: "Google",
+	"numberOfEmployees": 6734,
+	"headquarters": "San Franscisco",
+	"logo": "googleLogo.png",
+	"homepage": "http://www.google.com"
+};
+
 function App() {
-  return (
-    <div className="App">
-    hello
-    </div>
-  )
+
+	return (
+		<div className="App">
+			<div className="company">
+				<div className="name">Name: {company.name}</div>
+				<div className="numberOfEmployees">Employees: {company.numberOfEmployees}</div>
+				<div className="headquarters">Headquarters: {company.headquarters}</div>
+				
+
+			</div>
+		</div>
+	)
 }
 
 export default App
